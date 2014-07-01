25feebfe1b5568e75448792f25b3eb084ddeeb4b
Russell Kyle <russell.j.kyle@gmail.com>
2014-07-01 13:31:03 -0500

    Added the get_menu template tag to save a menu to a context variable.

--- templatetags/simple_menus_tags.py
+++ templatetags/simple_menus_tags.py
@@ -40,4 +40,36 @@ def show_menuitem(context, item, template_name='simple_menus/menuitem.html'):
     t = loader.get_template(template_name)
     return t.render(context)
 
+@register.tag
+def get_menu(parser, token):
+    # Usage: {% get_menu 'Menu Name' as 'VariableName' %}
+    
+    tokens = token.split_contents()
+    
+    if len(tokens) != 4:
+        raise template.TemplateSyntaxError, "%r tag should have 3 arguments" % (tokens[0],)
+    
+    lookup, skip, var_name = tokens[1:]
+        
+    if skip != 'as':
+        raise template.TemplateSyntaxError, "%r tag's second argument should be as without quotes."
+            
+    if (not is_quoted(lookup)) or (not is_quoted(var_name)):
+        raise template.TemplateSyntaxError, "%r tag's argument should be in quotes" % tokens[0]
+    
+    lookup = lookup[1:-1]
+        
+    return GetMenu(lookup, var_name[1:-1])
 
+class GetMenu(template.Node):
+    def __init__(self, lookup, option, var_name):
+        self.lookup = lookup
+        self.var_name = var_name
+    
+    def render(self, context):
+        try:
+            x = Menu.objects.get(title=self.lookup)
+        except:
+            x = None
+        context[self.var_name] = x            
+        return ''
